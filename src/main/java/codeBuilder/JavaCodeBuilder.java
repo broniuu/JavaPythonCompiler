d2ab60b4c5dafa2d4680b662e936d60851ebb3b1
broni <broniekf@gmail,com>
2023-05-04 22:10:09 +0200

    remove unused methods

--- src/main/java/codeBuilder/JavaCodeBuilder.java
+++ src/main/java/codeBuilder/JavaCodeBuilder.java
@@ -11,23 +11,19 @@ public class JavaCodeBuilder {
         this.currentTabsNumber = currentTabsNumber;
         return this;
     }
-
     public JavaCodeBuilder append(String string){
         this.stringBuilder.append(string);
         return this;
     }
-
     public JavaCodeBuilder append(JavaCodeBuilder codeBuilder){
         this.stringBuilder.append(codeBuilder.stringBuilder);
         return this;
     }
-
     public JavaCodeBuilder appendFunctionCall(String functionName, List<JavaCodeBuilder> arguments){
         return this.append(functionName)
                 .appendArgsOrParams(arguments)
                 .append(";");
     }
-
     private <T> JavaCodeBuilder appendArgsOrParams(List<T> argsOrParams) {
         stringBuilder.append("(");
         if (argsOrParams.size() > 0){
@@ -41,7 +37,6 @@ public class JavaCodeBuilder {
         stringBuilder.append(")");
         return this;
     }
-
     public JavaCodeBuilder appendFunctionDeclarations(List<JavaFunctionDeclaration> functionDeclaration)
     {
         functionDeclaration.forEach(this::appendFunctionDeclaration);
@@ -60,7 +55,6 @@ public class JavaCodeBuilder {
                 .append(emptyFunctionComment)
                 .append("}\n");
     }
-
     public JavaCodeBuilder appendFirstLine(JavaCodeBuilder codeBuilder) {
         return append(codeBuilder).appendNewLine();
     }
@@ -74,17 +68,14 @@ public class JavaCodeBuilder {
         stringBuilder.append("\t".repeat(Math.max(0, tabsNumber)));
         return this;
     }
-
     public JavaCodeBuilder appendTabs() {
         stringBuilder.append("\t".repeat(Math.max(0, currentTabsNumber)));
         return this;
     }
-
     public JavaCodeBuilder appendNewLine(){
         stringBuilder.append("\n");
         return this;
     }
-
     public  JavaCodeBuilder appendWhile(String condition, JavaCodeBuilder... lines) {
         return this.append("while (")
                 .append(condition)
@@ -92,13 +83,6 @@ public class JavaCodeBuilder {
                 .appendCodeBlockLines(lines)
                 .append("}");
     }
-    public JavaCodeBuilder appendStartWhile(String condition){
-        stringBuilder.append("while (")
-                .append(condition)
-                .append(") {");
-        return this;
-    }
-
     public JavaCodeBuilder appendThread(int threadNumber, JavaCodeBuilder... lines){
         return this.append("Thread thread_")
                 .append(String.valueOf(threadNumber))
@@ -107,19 +91,16 @@ public class JavaCodeBuilder {
                 .appendTabs().
                 append("});");
     }
-
     public JavaCodeBuilder appendStartThread(int threadNumber){
         return this.appendTabs()
                 .append("thread_")
                 .append(String.valueOf(threadNumber))
                 .append(".start();");
     }
-
     public JavaCodeBuilder appendClosedBracket(){
         stringBuilder.append("}");
         return this;
     }
-
     public JavaCodeBuilder appendIf(String condition, JavaCodeBuilder... lines){
         return this.append("if (")
                 .append(condition)
@@ -127,7 +108,6 @@ public class JavaCodeBuilder {
                 .appendCodeBlockLines(lines)
                 .appendTabs().append("}");
     }
-
     public JavaCodeBuilder appendElse(JavaCodeBuilder... lines) {
         return this.append(" else {\n")
                 .appendCodeBlockLines(lines)
@@ -141,7 +121,6 @@ public class JavaCodeBuilder {
                 .append(condition)
                 .append(");\n");
     }
-
     public JavaCodeBuilder appendCodeBlockLines(JavaCodeBuilder... lines){
         ++currentTabsNumber;
         for (JavaCodeBuilder instruction : lines) {
